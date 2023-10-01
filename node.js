3c1ce31dfaaa6ea68fd33df1c585111dc4d9d22d
roopxx <127219908+roopxx@users.noreply.github.com>
2023-10-01 13:04:49 +0530

    Add comments explaining Node class properties

--- node.js
+++ node.js
@@ -1,8 +1,13 @@
+// Define a Node class.
 class Node {
   constructor(value) {
+    // Initialize the data property with the provided value, or null if no value is provided.
     this.data = null || value;
+
+    // Initialize the nextNode property as null, representing the next node in the linked list.
     this.nextNode = null;
   }
 }
 
+// Export the Node class to make it available for use in other modules.
 export default Node;
